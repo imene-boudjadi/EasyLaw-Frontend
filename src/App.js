376f0a3775cb7f40d606440658fd2f7d78d9751d
yasmineghouar <ky_ghouar@esi.dz>
2024-04-08 00:56:42 +0100

    yasmine1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import './App.css';
-import { BrowserRouter as Router , Route , Routes } from 'react-router-dom';
-import HomePage from './Pages/homePage'
-import NewsPage from './Pages/newsPage'
-import AidePage from './Pages/aidePage'
-import OffresPage from './Pages/offresPage'
-import DetailNews from './Pages/detailPage'
-
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './Pages/homePage';
+import NewsPage from './Pages/newsPage';
+import AidePage from './Pages/aidePage';
+import OffresPage from './Pages/offresPage';
+import DetailNews from './Pages/detailPage';
+import ProfilePage from './Pages/profilePage'; 
+import NotifPage from './Pages/notifPage';
 
 function App() {
   return (
     <div className="App">
- <Router>
+      <Router>
         <Routes>
-          <Route path='/' element={<HomePage/>}/>
-          <Route path='/Aide' element={<AidePage/>}/>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/Aide' element={<AidePage />} />
           <Route path="/News/DetailNews/:idNews" element={<DetailNews />} />
-          <Route path='/News' element={<NewsPage/>}/>
-          <Route path='/Offres' element={<OffresPage/>}/>
+          <Route path='/News' element={<NewsPage />} />
+          <Route path='/Offres' element={<OffresPage />} />
+          <Route path='/ProfilePage' element={<ProfilePage />} />
+          <Route path='/NotifPage' element={<NotifPage />} />
         </Routes>
-       </Router>    </div>
+      </Router>
+    </div>
   );
 }
 
